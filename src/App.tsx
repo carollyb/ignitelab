5581e9800c2dd75e979c83754ca0b5c62bd5258b
carollyb <carolly.barbosa@gmail.com>
2022-10-13 22:24:57 -0300

    :sparkles: adds first component to storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './styles/global.css'
 
 export function App() {
-
   return (
-    <h1 className='font-bold text-2xl text-violet-600'>Hello world</h1>
+    <>
+      <h1 className='font-bold text-2xl text-violet-800'>Hello world</h1>
+      <button className='bg-violet-800 font-medium p-4 rounded hover:bg-violet-500'>Enviar</button>
+    </>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,23 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+      'violet-800': '#7440b8',
+      'violet-500': '#a660ff',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'

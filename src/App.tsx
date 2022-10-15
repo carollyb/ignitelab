4c282bcd7f5e07568ddb7c52d686bb9cb9aa12af
carollyb <carolly.barbosa@gmail.com>
2022-10-14 23:33:17 -0300

    :sparkles: creates button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Button } from './components/Button'
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
+    <div className='bg-gray-800'>
       <h1 className='font-bold text-2xl text-violet-800'>Hello world</h1>
-      <button className='bg-violet-800 font-medium p-4 rounded hover:bg-violet-500'>Enviar</button>
-    </>
+      <Button>Teste</Button>
+    </div>
   )
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx'
+import { ReactNode } from 'react';
+
+export interface ButtonProps {
+  children: ReactNode
+  asChild?: boolean
+}
+
+export function Button({ children, asChild }: ButtonProps) {
+  const Comp = asChild ? Slot : 'button'
+  return (
+    <Comp className={clsx(
+      "p-4 bg-violet-800 rounded font-semibold text-gray-800 text-sm w-full transition-colors hover:bg-violet-500 focus:ring ring-offset-1 focus:ring-violet-800",
+      )}
+      >
+        {children}
+    </Comp>
+  );
+}
